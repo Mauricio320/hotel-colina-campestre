9d28200c2e499bb28fa9fc9c7de565a4074062d6
Mauricio Triana <mtriana@pedrodevaldivia.cl>
2026-02-28 15:27:35 -0500

    feature/(landing-page-editor) se agrega la funcionalidad para una columnas

--- src/pages/craft-tutorial/example/pages/index.tsx
+++ src/pages/craft-tutorial/example/pages/index.tsx
@@ -1,41 +1,26 @@
-import { Editor, Frame, Element } from '@craftjs/core';
-import { createTheme, ThemeProvider } from '@mui/material';
-import { NextSeo } from 'next-seo';
-import React from 'react';
+import { Editor, Frame, Element } from "@craftjs/core";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-import { Viewport, RenderNode } from '../components/editor';
-import { Container, Text } from '../components/selectors';
-import { Button } from '../components/selectors/Button';
-import { Custom1, OnlyButtons } from '../components/selectors/Custom1';
-import { Custom2, Custom2VideoDrop } from '../components/selectors/Custom2';
-import { Custom3, Custom3BtnDrop } from '../components/selectors/Custom3';
-import { Video } from '../components/selectors/Video';
+import React from "react";
+
+import { Viewport, RenderNode } from "../components/editor";
+import { Container, Text } from "../components/selectors";
+import { Button } from "../components/selectors/Button";
+import { Custom1, OnlyButtons } from "../components/selectors/Custom1";
+import { Custom2, Custom2VideoDrop } from "../components/selectors/Custom2";
+import { Custom3, Custom3BtnDrop } from "../components/selectors/Custom3";
+import { Video } from "../components/selectors/Video";
 
 const theme = createTheme({
   typography: {
-    fontFamily: [
-      'acumin-pro',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-    ].join(','),
+    fontFamily: ["acumin-pro", "Roboto", '"Helvetica Neue"', "Arial", "sans-serif"].join(","),
   },
 });
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="h-full h-screen">
-        <NextSeo
-          title="Craft.js"
-          description="A React framework for building drag-n-drop page editors."
-          canonical="https://craft.js.org/"
-          twitter={{
-            site: 'craft.js.org',
-            cardType: 'summary_large_image',
-          }}
-        />
+      <div className="h-full">
         <Editor
           resolver={{
             Container,
@@ -60,8 +45,8 @@ function App() {
                 width="800px"
                 height="auto"
                 background={{ r: 255, g: 255, b: 255, a: 1 }}
-                padding={['40', '40', '40', '40']}
-                custom={{ displayName: 'App' }}
+                padding={["40", "40", "40", "40"]}
+                custom={{ displayName: "App" }}
               >
                 <Element
                   canvas
@@ -69,17 +54,17 @@ function App() {
                   flexDirection="row"
                   width="100%"
                   height="auto"
-                  padding={['40', '40', '40', '40']}
-                  margin={['0', '0', '40', '0']}
-                  custom={{ displayName: 'Introduction' }}
+                  padding={["40", "40", "40", "40"]}
+                  margin={["0", "0", "40", "0"]}
+                  custom={{ displayName: "Introduction" }}
                 >
                   <Element
                     canvas
                     is={Container}
                     width="40%"
                     height="100%"
-                    padding={['0', '20', '0', '20']}
-                    custom={{ displayName: 'Heading' }}
+                    padding={["0", "20", "0", "20"]}
+                    custom={{ displayName: "Heading" }}
                   >
                     <Text
                       fontSize="23"
@@ -92,8 +77,8 @@ function App() {
                     is={Container}
                     width="60%"
                     height="100%"
-                    padding={['0', '20', '0', '20']}
-                    custom={{ displayName: 'Description' }}
+                    padding={["0", "20", "0", "20"]}
+                    custom={{ displayName: "Description" }}
                   >
                     <Text
                       fontSize="14"
@@ -110,9 +95,9 @@ function App() {
                   flexDirection="column"
                   width="100%"
                   height="auto"
-                  padding={['40', '40', '40', '40']}
-                  margin={['0', '0', '40', '0']}
-                  custom={{ displayName: 'ComplexSection' }}
+                  padding={["40", "40", "40", "40"]}
+                  margin={["0", "0", "40", "0"]}
+                  custom={{ displayName: "ComplexSection" }}
                 >
                   <Element
                     canvas
@@ -124,11 +109,11 @@ function App() {
                     }}
                     is={Container}
                     flexDirection="row"
-                    margin={['0', '0', '0', '0']}
+                    margin={["0", "0", "0", "0"]}
                     width="100%"
                     height="auto"
                     alignItems="center"
-                    custom={{ displayName: 'Wrapper' }}
+                    custom={{ displayName: "Wrapper" }}
                   >
                     <Element
                       canvas
@@ -140,11 +125,11 @@ function App() {
                       }}
                       is={Container}
                       alignItems="center"
-                      padding={['0', '0', '0', '0']}
+                      padding={["0", "0", "0", "0"]}
                       flexDirection="row"
                       width="350px"
                       height="250px"
-                      custom={{ displayName: 'Square' }}
+                      custom={{ displayName: "Square" }}
                     >
                       <Element
                         canvas
@@ -160,8 +145,8 @@ function App() {
                         shadow={25}
                         width="90%"
                         height="90%"
-                        padding={['10', '20', '10', '20']}
-                        custom={{ displayName: 'Outer' }}
+                        padding={["10", "20", "10", "20"]}
+                        custom={{ displayName: "Outer" }}
                       >
                         <Element
                           canvas
@@ -177,8 +162,8 @@ function App() {
                           shadow={50}
                           width="80%"
                           height="80%"
-                          padding={['10', '20', '10', '20']}
-                          custom={{ displayName: 'Middle' }}
+                          padding={["10", "20", "10", "20"]}
+                          custom={{ displayName: "Middle" }}
                         >
                           <Element
                             canvas
@@ -194,8 +179,8 @@ function App() {
                             shadow={50}
                             width="60%"
                             height="60%"
-                            padding={['10', '20', '10', '20']}
-                            custom={{ displayName: 'Inner' }}
+                            padding={["10", "20", "10", "20"]}
+                            custom={{ displayName: "Inner" }}
                           />
                         </Element>
                       </Element>
@@ -209,30 +194,30 @@ function App() {
                         a: 0,
                       }}
                       is={Container}
-                      padding={['0', '0', '0', '20']}
+                      padding={["0", "0", "0", "20"]}
                       flexDirection="column"
                       width="55%"
                       height="100%"
                       fillSpace="yes"
-                      custom={{ displayName: 'Content' }}
+                      custom={{ displayName: "Content" }}
                     >
                       <Text
                         color={{
-                          r: '255',
-                          g: '255',
-                          b: '255',
-                          a: '1',
+                          r: "255",
+                          g: "255",
+                          b: "255",
+                          a: "1",
                         }}
-                        margin={['0', '0', '18', '0']}
+                        margin={["0", "0", "18", "0"]}
                         fontSize="20"
                         text="Design complex components"
                       ></Text>
                       <Text
                         color={{
-                          r: '255',
-                          g: '255',
-                          b: '255',
-                          a: '0.8',
+                          r: "255",
+                          g: "255",
+                          b: "255",
+                          a: "0.8",
                         }}
                         fontSize="14"
                         fontWeight="400"
@@ -253,9 +238,9 @@ function App() {
                   flexDirection="column"
                   width="100%"
                   height="auto"
-                  padding={['40', '40', '40', '40']}
-                  margin={['0', '0', '40', '0']}
-                  custom={{ displayName: 'Programmatic' }}
+                  padding={["40", "40", "40", "40"]}
+                  margin={["0", "0", "40", "0"]}
+                  custom={{ displayName: "" }}
                 >
                   <Element
                     canvas
@@ -267,17 +252,17 @@ function App() {
                     }}
                     is={Container}
                     flexDirection="column"
-                    margin={['0,', '0', '20', '0']}
+                    margin={["0,", "0", "20", "0"]}
                     width="100%"
                     height="auto"
-                    custom={{ displayName: 'Heading' }}
+                    custom={{ displayName: "Heading" }}
                   >
                     <Text
                       color={{
-                        r: '46',
-                        g: '47',
-                        b: '47',
-                        a: '1',
+                        r: "46",
+                        g: "47",
+                        b: "47",
+                        a: "1",
                       }}
                       fontSize="23"
                       text="Programmatic drag &amp; drop"
@@ -298,10 +283,10 @@ function App() {
                     }}
                     is={Container}
                     flexDirection="row"
-                    margin={['30', '0', '0', '0']}
+                    margin={["30", "0", "0", "0"]}
                     width="100%"
                     height="auto"
-                    custom={{ displayName: 'Content' }}
+                    custom={{ displayName: "Content" }}
                   >
                     <Element
                       canvas
@@ -312,10 +297,10 @@ function App() {
                         a: 0,
                       }}
                       is={Container}
-                      padding={['0', '20', '0', '0']}
+                      padding={["0", "20", "0", "0"]}
                       flexDirection="row"
                       width="45%"
-                      custom={{ displayName: 'Left' }}
+                      custom={{ displayName: "Left" }}
                     >
                       <Custom1
                         background={{
@@ -326,8 +311,8 @@ function App() {
                         }}
                         height="auto"
                         width="100%"
-                        padding={['20', '20', '20', '20']}
-                        margin={['0', '0', '0', '0']}
+                        padding={["20", "20", "20", "20"]}
+                        margin={["0", "0", "0", "0"]}
                         shadow={40}
                       />
                     </Element>
@@ -340,10 +325,10 @@ function App() {
                         a: 0,
                       }}
                       is={Container}
-                      padding={['0', '0', '0', '20']}
+                      padding={["0", "0", "0", "20"]}
                       flexDirection="column"
                       width="55%"
-                      custom={{ displayName: 'Right' }}
+                      custom={{ displayName: "Right" }}
                     >
                       <Custom2
                         background={{
@@ -354,8 +339,8 @@ function App() {
                         }}
                         height="125px"
                         width="100%"
-                        padding={['0', '0', '0', '20']}
-                        margin={['0', '0', '0', '0']}
+                        padding={["0", "0", "0", "20"]}
+                        margin={["0", "0", "0", "0"]}
                         shadow={40}
                         flexDirection="row"
                         alignItems="center"
@@ -369,8 +354,8 @@ function App() {
                         }}
                         height="auto"
                         width="100%"
-                        padding={['20', '20', '20', '20']}
-                        margin={['20', '0', '0', '0']}
+                        padding={["20", "20", "20", "20"]}
+                        margin={["20", "0", "0", "0"]}
                         shadow={40}
                         flexDirection="column"
                       />
